9223ec65127cfe2c5e5368a86b52285a830cf643
Evan Anderson <evananderson@aospa.co>
2017-06-04 21:35:04 -0500

    ParanoidHub: Version: Support tags in version name
    * Hub didn't expect the extra tag of DEV, PRESS, or RELEASE in the version
      string, and thus didn't correctly parse the build version. This change adds
      support for these tags. This change ignores the DEV tag for update
      purposes because only PRESS and RELEASE builds are eligible for OTA.
    
    Ticket: NOUGAT-60
    
    Change-Id: I0689448b6aebe35339791f2f238508da05b2370b
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- src/com/paranoid/paranoidhub/utils/Version.java
+++ src/com/paranoid/paranoidhub/utils/Version.java
@@ -7,35 +7,53 @@ import java.io.Serializable;
  * Class to manage different versions
  * <p/>
  * Format<br>
- * pa_A-B-C.D.E-F.zip<br>
+ * pa_A-B-C.D.E-F-G.zip<br>
  * where<br>
  * A = device name, required<br>
  * B = extra information, not required (for gapps)<br>
  * C = major, integer from 0 to n, required<br>
  * D = minor, integer from 0 to 9, required<br>
  * E = maintenance, integer from 0 to n, not required<br>
- * F = date, YYYYMMDD, not required
+ * F = tag. Expected value is DEV, PRESS, or RELEASE <br>
+ * G = date, YYYYMMDD, not required
+ * Any additions past G will be ignored
  * <p/>
  * All the default values not specified above are 0
  * <p/>
  * Examples<br>
- * pa_angler-6.0.1-20160616.zip<br>
+ * pa_angler-7.1.2-RELEASE-20170620-signed.zip<br>
  */
 public class Version implements Serializable {
 
     private static final String TAG = "Hub/Version";
 
+    private static final String DEV_RELEASE_TAG = "DEV";
+    private static final String PRESS_RELEASE_TAG = "PRESS";
+    private static final String PUB_RELEASE_TAG = "RELEASE";
+
     private int mMajor = 0;
     private int mMinor = 0;
     private int mMaintenance = 0;
 
-    private String mDate = "0";
+    private String mTag = "";
+    private String mDate = "";
 
     public Version(String version) {
-        this(version.split("-")[0], version.split("-")[1]);
+        String[] versionSplit = version.split("-");
+        if (versionSplit.length == 3) {
+            parseVersion(versionSplit[0], versionSplit[1], versionSplit[2]);
+        } else if (versionSplit.length == 2) {
+            parseVersion(versionSplit[0], "", versionSplit[1]);
+        } else {
+            Log.d(TAG, "Malformed version: " + version);
+        }
     }
 
     public Version(String version, String date) {
+        parseVersion(version, "", date);
+    }
+
+    private void parseVersion(String version, String tag, String date) {
         try {
             String[] parts = version.split("\\.");
             mMajor = Integer.parseInt(parts[0]);
@@ -45,6 +63,7 @@ public class Version implements Serializable {
             if (parts.length > 2) {
                 mMaintenance = Integer.parseInt(parts[2]);
             }
+            mTag = tag;
             mDate = date;
             if (Constants.DEBUG) Log.d(TAG, "got version: " + mMajor + "." + mMinor + "." + mMaintenance);
             if (Constants.DEBUG) Log.d(TAG, "got date: " + mDate);
@@ -69,6 +88,10 @@ public class Version implements Serializable {
         if (!v1.getDate().equals(v2.getDate())) {
             return v1.getDate().compareTo(v2.getDate());
         }
+        if (v1.getTag() != v2.getTag()) {
+            return v1.getTag().equals(PRESS_RELEASE_TAG) &&
+                   	v2.getTag().equals(PUB_RELEASE_TAG) ? -1 : 1;
+        }
         return 0;
     }
 
@@ -82,6 +105,10 @@ public class Version implements Serializable {
 
     public int getMaintenance() {
         return mMaintenance;
+    }
+
+    public String getTag() {
+        return mTag;
     }
 
     public String getDate() {
