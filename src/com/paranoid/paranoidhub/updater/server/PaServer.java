b30dff8bfe4b0488e8fd70f23bc25bcb6a025ae0
Hernán Castañón <herna@paranoidandroid.co>
2018-06-15 03:50:50 +0200

    ParanoidHub: modifications for new versioning
    Added BETA tag, and implemented new versioning reading.
    
    Change-Id: I1995d97c56a7b4beee54e5c77a848c2d7c323459
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- src/com/paranoid/paranoidhub/updater/server/PaServer.java
+++ src/com/paranoid/paranoidhub/updater/server/PaServer.java
@@ -38,7 +38,7 @@ public class PaServer implements Server {
 
     private static final String TAG = Constants.BASE_TAG + "PaServer";
 
-    private static final String URL = "http://api.aospa.co/updates/%s";
+    private static final String URL = "https://gitlab.com/herna/pa_try/raw/master/%s";
 
     private String mDevice = null;
     private String mError = null;
