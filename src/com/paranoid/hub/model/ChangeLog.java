743049e3a8b2eedbd4652b4fc532a42241be9380
Chris Crump <chris@cypheros.co>
2020-03-14 06:50:38 -0400

    ParanoidHub: Initial import of Hub3
    Hub3 is a completely rewritten version of ParanoidHub with a new user interface and advanced features. Hub3 also supports A/B installations natively as well as local installations and system downgrading
    
    Change-Id: I973f580066a4cd16a6e3dfab5fff5938d7a070d3

---
+++ src/com/paranoid/hub/model/ChangeLog.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.hub.model;
+
+public class ChangeLog {
+
+    private String mChangelog;
+
+    public ChangeLog() {
+    }
+
+    public String get() {
+        return mChangelog;
+    }
+
+    public void set(String changelog) {
+        mChangelog = changelog;
+    }
+}
