e9a07ce0855e7e80c0f69d0d37439b759e8cde76
Chris Crump <chris@cypheros.co>
2020-03-19 00:04:38 -0400

    ParanoidHub: Initial import of Hub3
    Hub3 is a completely rewritten version of ParanoidHub with a new user interface and advanced features. Hub3 also supports A/B installations natively as well as local installations and system downgrading
    
    Change-Id: I973f580066a4cd16a6e3dfab5fff5938d7a070d3

---
+++ src/com/paranoid/hub/controller/LocalUpdateController.java
@@ -0,0 +1,318 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.hub.controller;
+
+import android.content.Context;
+import android.annotation.NonNull;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+
+import androidx.documentfile.provider.DocumentFile;
+
+import com.paranoid.hub.HubController;
+import com.paranoid.hub.R;
+import com.paranoid.hub.misc.Constants;
+import com.paranoid.hub.model.Update;
+import com.paranoid.hub.model.UpdateInfo;
+import com.paranoid.hub.model.UpdateStatus;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LocalUpdateController {
+
+    private static final String TAG = "LocalUpdateController";
+
+    private static LocalUpdateController sInstance = null;
+
+    private final HubController mController;
+    private final Context mContext;
+
+    public LocalUpdateController(Context context, HubController controller) {
+        mController = controller;
+        mContext = context.getApplicationContext();
+    }
+
+    public static synchronized LocalUpdateController getInstance(Context context,
+            HubController controller) {
+        if (sInstance == null) {
+            sInstance = new LocalUpdateController(context, controller);
+        }
+        return sInstance;
+    }
+
+    public UpdateInfo buildUpdate(Uri uri) {
+        String path = getRealPath(mContext, uri);
+        Update update = new Update();
+        File file = new File(path);
+        update.setFile(file);
+        update.setName(file.getName());
+        update.setFileSize(file.length());
+        update.setTimestamp(getTimestamp(file.getName()));
+        update.setDownloadId(getDummyId(file.getName()));
+        update.setVersion(getVersion(file.getName()));
+        update.setPersistentStatus(UpdateStatus.Persistent.LOCAL_UPDATE);
+        return update;
+    }
+
+    public UpdateInfo getUpdate(Uri uri) {
+        UpdateInfo update = buildUpdate(uri);
+        return update;
+    }
+
+    private String getVersion(String fileName) {
+        String[] version = fileName.split("-");
+        return version[2];
+    }
+
+    private Long getTimestamp(String fileName) {
+        String[] version = fileName.split("-");
+        return Long.parseLong(version[4]);
+    }
+
+    private String getDummyId(String fileName) {
+        String[] version = fileName.split("-");
+        long timestamp = Long.parseLong(version[4]) * 2;
+        return Long.toString(timestamp);
+    }
+
+    public static String getRealPath(Context context, Uri uri) {
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) { // DownloadsProvider
+                DocumentFile fileD = DocumentFile.fromSingleUri(context, uri);
+                Log.d(TAG, "" + fileD.getName());
+                Log.d(TAG, "" + fileD.getType());
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                if (id != null && id.startsWith("raw:")) {
+                    return id.substring(4);
+                }
+
+                String[] contentUriPrefixesToTry = new String[]{
+                        "content://downloads/public_downloads",
+                        "content://downloads/my_downloads"
+                };
+
+                for (String contentUriPrefix : contentUriPrefixesToTry) {
+                    Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
+                    try {
+                        String path = getDataColumn(context, contentUri, null, null);
+                        if (path != null) {
+                            return path;
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+
+                // path could not be retrieved using ContentResolver, therefore copy file to accessible cache using streams
+                String fileName = getFileName(context, uri);
+                File cacheDir = getDocumentCacheDir(context);
+                File file = generateFileName(fileName, cacheDir);
+                String destinationPath = null;
+                if (file != null) {
+                    destinationPath = file.getAbsolutePath();
+                    saveFileFromUri(context, uri, destinationPath);
+                }
+                return destinationPath;
+            } else if (isMediaDocument(uri)) { // MediaProvider
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) { // MediaStore (and general)
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) { // File
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static String getFileName(Context context, Uri uri) {
+        String mimeType = context.getContentResolver().getType(uri);
+        String filename = null;
+
+        if (mimeType == null && context != null) {
+            String path = getRealPath(context, uri);
+            if (path == null) {
+                filename = getName(uri.toString());
+            } else {
+                File file = new File(path);
+                filename = file.getName();
+            }
+        } else {
+            Cursor returnCursor = context.getContentResolver().query(uri, null, null, null, null);
+            if (returnCursor != null) {
+                int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                returnCursor.moveToFirst();
+                filename = returnCursor.getString(nameIndex);
+                returnCursor.close();
+            }
+        }
+
+        return filename;
+    }
+
+    public static String getName(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int index = filename.lastIndexOf('/');
+        return filename.substring(index + 1);
+    }
+
+    public static File getDocumentCacheDir(@NonNull Context context) {
+        File dir = new File(context.getCacheDir(), "documents");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dir;
+    }
+
+    public static File generateFileName(String name, File directory) {
+        if (name == null) {
+            return null;
+        }
+        File file = new File(directory, name);
+
+        if (file.exists()) {
+            String fileName = name;
+            String extension = "";
+            int dotIndex = name.lastIndexOf('.');
+            if (dotIndex > 0) {
+                fileName = name.substring(0, dotIndex);
+                extension = name.substring(dotIndex);
+            }
+
+            int index = 0;
+            while (file.exists()) {
+                index++;
+                name = fileName + '(' + index + ')' + extension;
+                file = new File(directory, name);
+            }
+        }
+
+        try {
+            if (!file.createNewFile()) {
+                return null;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return file;
+    }
+
+    private static void saveFileFromUri(Context context, Uri uri, String destinationPath) {
+        InputStream is = null;
+        BufferedOutputStream bos = null;
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            bos = new BufferedOutputStream(new FileOutputStream(destinationPath, false));
+            byte[] buf = new byte[1024];
+            is.read(buf);
+            do {
+                bos.write(buf);
+            } while (is.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/paranoid/hub/download/ClientConnector.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.hub.download;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientConnector implements DownloadClient.DownloadCallback {
+
+    private static final String TAG = "ClientConnector";
+
+    private List<ConnectorListener> mListeners = new ArrayList<>();
+
+    private Context mContext;
+    private DownloadClient mClient;
+    private File mJson;
+
+    public interface ConnectorListener {
+        void onClientStatusFailure(boolean cancelled);
+        void onClientStatusResponse(int statusCode, String url, DownloadClient.Headers headers);
+        void onClientStatusSuccess(File oldJson, File newJson);
+    }
+
+    public ClientConnector(Context context) {
+        mContext = context;
+    }
+
+    public void insert(File oldJson, File newJson, String url) {
+        Log.d(TAG, "Old update table: " + oldJson.getName() + "New update table: " + newJson.getName());
+        mJson = oldJson;
+        mClient = null;
+        try {
+            mClient = new DownloadClient.Builder()
+                    .setUrl(url)
+                    .setDestination(newJson)
+                    .setDownloadCallback(this)
+                    .build();
+        } catch (IOException exception) {
+            Log.d(TAG, "Could not build download client");
+            return;
+        }
+    }
+
+    public void start() {
+        mClient.start();
+    }
+
+    public void addClientStatusListener(ConnectorListener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeClientStatusListener(ConnectorListener listener) {
+        mListeners.remove(listener);
+    }
+
+    private void notifyClientStatusFailure(boolean cancelled) {
+        for (ConnectorListener listener : mListeners) {
+            listener.onClientStatusFailure(cancelled);
+        }
+    }
+
+    private void notifyClientStatusResponse(int statusCode, String url,
+        DownloadClient.Headers headers) {
+        for (ConnectorListener listener : mListeners) {
+            listener.onClientStatusResponse(statusCode, url, headers);
+        }
+    }
+
+    private void notifyClientStatusSuccess(File oldJson, File newJson) {
+        for (ConnectorListener listener : mListeners) {
+            listener.onClientStatusSuccess(oldJson, newJson);
+        }
+    }
+
+    @Override
+    public void onFailure(final boolean cancelled) {
+        Log.e(TAG, "Could not download updates");
+        notifyClientStatusFailure(cancelled);
+    }
+
+    @Override
+    public void onResponse(int statusCode, String url,
+        DownloadClient.Headers headers) {
+        notifyClientStatusResponse(statusCode, url, headers);
+    }
+
+    @Override
+    public void onSuccess(File destination) {
+        notifyClientStatusSuccess(mJson, destination);
+    }
+    
+}
