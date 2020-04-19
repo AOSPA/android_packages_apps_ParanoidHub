1a58e8dcc47aa0a059e5740fc55531c59fecc70c
Chris Crump <chris@cypheros.co>
2020-04-19 02:59:22 -0400

    ParanoidHub: Fix last check time not updating
    Change-Id: I000a4a0d577c319965e3eae3059b4a18795a00fb

--- src/com/paranoid/hub/HubUpdateManager.java
+++ src/com/paranoid/hub/HubUpdateManager.java
@@ -156,9 +156,6 @@ public class HubUpdateManager implements ClientConnector.ConnectorListener {
                 Log.d(TAG, "Requesting update..");
                 try {
                     syncUpdate(newJson);
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-                    long millis = System.currentTimeMillis();
-                    prefs.edit().putLong(Constants.PREF_LAST_UPDATE_CHECK, millis).apply();
                     if (oldJson.exists() && UpdatePresenter.isNewUpdate(mContext, oldJson, newJson, mRolloutContractor.isReady())) {
                         UpdateCheckReceiver.updateRepeatingUpdatesCheck(mContext);
                     }
@@ -179,6 +176,9 @@ public class HubUpdateManager implements ClientConnector.ConnectorListener {
 
     private void syncUpdate(File json) throws IOException, JSONException {
         if (mEnabled) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+            long millis = System.currentTimeMillis();
+            prefs.edit().putLong(Constants.PREF_LAST_UPDATE_CHECK, millis).apply();
             Log.d(TAG, "Syncing requested update");
             UpdateInfo update = UpdatePresenter.matchMakeJson(mContext, json);
             if (update != null) {
