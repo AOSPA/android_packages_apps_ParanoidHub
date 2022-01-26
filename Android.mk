38a0c66685e32b07a9395608e35c0271db971eca
Chris Crump <chriscrawford893@gmail.com>
2022-01-26 00:45:14 -0500

    Initial refactor for ParanoidHub
    ParanoidHub is now official based on Lineage's Updater for it's background infrastructure
    
    Change-Id: I550e5838f50f038e6219a19cc35f6c4fdfaf7b2e

--- Android.mk
+++ Android.mk
@@ -21,19 +21,19 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := Updater
+LOCAL_PACKAGE_NAME := ParanoidHub
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.updater.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_co.aospa.paranoidhub.xml
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp_whitelist_org.lineageos.updater.xml
+LOCAL_MODULE := privapp_whitelist_co.aospa.paranoidhub.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
@@ -43,7 +43,7 @@ include $(BUILD_PREBUILT)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := UpdaterStudio
+LOCAL_MODULE := ParanoidHubStudio
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_SUFFIX := -timestamp
 updater_system_deps := $(call java-lib-deps,framework)
