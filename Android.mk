645477c18a6808e309789a4a520ee0c5866095db
Gabriele M <moto.falcon.git@gmail.com>
2022-04-21 10:37:45 +0530

    ParanoidHub: allow building framework jar for gradle support
    Change-Id: Ife17618ad83a9d40b94581c3c13ff22359dc29fd
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- Android.mk
+++ Android.mk
@@ -1,2 +1,18 @@
 LOCAL_PATH:= $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := UpdaterStudio
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+updater_system_deps := $(call java-lib-deps,framework)
+updater_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(updater_system_deps)
+	$(hide) mkdir -p $(updater_system_libs_path)
+	$(hide) rm -rf $(updater_system_libs_path)/*.jar
+	$(hide) cp $(updater_system_deps) $(updater_system_libs_path)/framework.jar
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
