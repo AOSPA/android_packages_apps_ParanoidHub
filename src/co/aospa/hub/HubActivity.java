1190cc4400c8903c8bd5dad37f9b37ceab8c9c0a
Hernán Castañón <herna@paranoidandroid.co>
2022-09-08 18:39:34 +0200

    ParanoidHub: show Paranoid Android version properly.
    Beta shows Sapphire 1 when it should be Sapphire Beta 1.
    
    Adapt in case we support another variants in the future.
    
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>
    Change-Id: I1f7140e520dd468a6623d1152fb22fcb63bbcb2e

--- src/co/aospa/hub/HubActivity.java
+++ src/co/aospa/hub/HubActivity.java
@@ -480,9 +480,15 @@ public class HubActivity extends AppCompatActivity implements View.OnClickListen
             mVersionContainer.setVisibility(View.VISIBLE);
             final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
             long lastChecked = prefs.getLong(Constants.PREF_LAST_UPDATE_CHECK, -1) / 1000;
+            Version version = new Version(context, update);
+            boolean isTestersUpdate = version.isTestersUpdate();
             mVersionHeader.setTypeface(mVersionHeader.getTypeface(), Typeface.NORMAL);
             mVersionHeader.setText(String.format(
-                    getResources().getString(R.string.no_updates_text), 
+                    !isTestersUpdate ?
+                    getResources().getString(R.string.no_updates_text_no_release),
+                    Version.getMajor(), Version.getBuildType(), Version.getMinor(),
+                    getAndroidSpl(), StringGenerator.getTimeLocalized(this, lastChecked) :
+                    getResources().getString(R.string.no_updates_text),
                     Version.getMajor(), Version.getMinor(),
                     getAndroidSpl(), StringGenerator.getTimeLocalized(this, lastChecked)));
         }
