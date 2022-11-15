eddc87215f84d97d39ee245721987590d73c848b
Chris Crump <chriscrawford893@gmail.com>
2022-11-15 04:44:53 -0500

    ParanoidHub: Update to Hub 5.0
    Hub has been rewritten completely from scratch with the goal to introduce a lighter frontend and backend system.
    
    The results of the rewrite has reduced code duplication by 50% and created a solid foundation for the UI. The UI states and tasks are all broken up into individual components. A component stores the relevant information for each state and publishes the appropriate state. This on it's own keeps the UI from being in a buggy state for most usage cases.
    
    Components and States are instances of a single interface which helps shrink code duplication as well.
    
    Change-Id: Ie3f13cb5a58591e2e683c02c10b5ce99db3627c7

---
+++ src/co/aospa/hub/components/OtaConfigComponent.java
@@ -0,0 +1,89 @@
+package co.aospa.hub.components;
+
+import java.io.File;
+
+public class OtaConfigComponent extends Component {
+
+    private boolean mEnabled;
+    private boolean mWhitelistOnly;
+
+    public void setOtaEnabled(String otaEnabled) {
+        boolean enabled = Boolean.parseBoolean(otaEnabled);
+        if (enabled != mEnabled) {
+            mEnabled = enabled;
+        }
+    }
+
+    public void setOtaWhitelistOnly(String otaWhitelistOnly) {
+        boolean whitelistOnly = Boolean.parseBoolean(otaWhitelistOnly);
+        if (whitelistOnly != mWhitelistOnly) {
+            mWhitelistOnly = whitelistOnly;
+        }
+    }
+
+    public boolean isEnabledFromServer() {
+        return mEnabled;
+    }
+
+    public boolean isWhitelistOnly() {
+        return mWhitelistOnly;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public void setVersion(String version) { }
+
+    @Override
+    public String getVersionNumber() {
+        return null;
+    }
+
+    @Override
+    public void setVersionNumber(String versionNumber) { }
+
+    @Override
+    public String getBuildType() {
+        return null;
+    }
+
+    @Override
+    public void setBuildType(String buildType) { }
+
+    @Override
+    public String getAndroidVersion() {
+        return null;
+    }
+
+    @Override
+    public void setAndroidVersion(String androidVersion) {
+
+    }
+
+    @Override
+    public String getAndroidSpl() {
+        return null;
+    }
+
+    @Override
+    public void setAndroidSpl(String androidSpl) { }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(String id) { }
+
+    @Override
+    public File getFile() {
+        return null;
+    }
+
+    @Override
+    public void setFile(File file) { }
+}
---
+++ src/co/aospa/hub/controllers/NotificationController.java
@@ -0,0 +1,117 @@
+package co.aospa.hub.controllers;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.text.format.Formatter;
+
+import androidx.core.app.NotificationCompat;
+
+import co.aospa.hub.R;
+import co.aospa.hub.components.UpdateComponent;
+import co.aospa.hub.ui.activities.HubActivity;
+
+public class NotificationController {
+
+    private static final int NOTIFICATION_ID = 10;
+
+    private final Context mContext;
+
+    public @interface NotificationType {
+        int AVAILABLE = 0;
+        int COMPLETED = 1;
+        int REBOOT = 2;
+        int TESTERS = 2;
+    }
+
+    public NotificationController(Context context) {
+        mContext = context;
+    }
+
+    public void showNotification(int notificationType, UpdateComponent component) {
+        NotificationChannel notificationChannel = new NotificationChannel(
+                "system_updates_notification_channel",
+                mContext.getString(R.string.system_update_notification_channel),
+                NotificationManager.IMPORTANCE_HIGH);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext,
+                "system_updates_notification_channel")
+                .setContentIntent(getIntent(notificationType))
+                .setSmallIcon(R.drawable.ic_system_update_24dp)
+                .setContentTitle(getContentTitle(notificationType, component))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                        getContentTitle(notificationType, component)))
+                .setContentText(getContentText(notificationType))
+                .setOngoing(getOngoing(notificationType));
+        NotificationManager nM = mContext.getSystemService(NotificationManager.class);
+        nM.createNotificationChannel(notificationChannel);
+        nM.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    public void cancelNotification() {
+        NotificationManager nM = mContext.getSystemService(NotificationManager.class);
+        nM.cancel(NOTIFICATION_ID);
+    }
+
+    private PendingIntent getIntent(int notificationType) {
+        Intent notificationIntent = new Intent(mContext, HubActivity.class);
+        if (notificationType == NotificationType.TESTERS) {
+            // Todo add testers activity intent
+        }
+        return PendingIntent.getActivity(mContext, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+    }
+
+    private String getContentTitle(
+            int notificationType, UpdateComponent component) {
+        String contentTitle = null;
+        switch (notificationType) {
+            case NotificationType.AVAILABLE:
+                contentTitle = getContentTitleFromComponent(component);
+                break;
+            case  NotificationType.COMPLETED:
+                contentTitle = mContext.getResources().getString(
+                        R.string.system_update_notification_update_completed);
+                break;
+            case  NotificationType.REBOOT:
+                contentTitle = mContext.getResources().getString(
+                        R.string.system_update_notification_update_finished_reboot);
+                break;
+        }
+        return contentTitle;
+    }
+
+    private String getContentText(int notificationType) {
+        String contentText = null;
+        switch (notificationType) {
+            case NotificationType.AVAILABLE:
+                contentText = mContext.getResources().getString(
+                        R.string.system_update_notification_update_available_desc);
+                break;
+            case  NotificationType.COMPLETED:
+                contentText = mContext.getResources().getString(
+                        R.string.system_update_notification_update_completed_desc);
+                break;
+            case  NotificationType.REBOOT:
+                contentText = mContext.getResources().getString(
+                        R.string.system_update_notification_update_finished_reboot_desc);
+                break;
+        }
+        return contentText;
+    }
+
+    private boolean getOngoing(int notificationType) {
+        return notificationType == NotificationType.AVAILABLE;
+    }
+
+    private String getContentTitleFromComponent(
+            UpdateComponent component) {
+        return component != null ? String.format(mContext.getResources().getString(
+                        R.string.system_update_notification_update_available),
+                component.getVersion(),
+                component.getVersionNumber())
+                : mContext.getResources().getString(
+                R.string.system_update_notification_update_available_default);
+    }
+}
